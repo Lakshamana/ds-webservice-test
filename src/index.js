419e692f3df5e44278753ecd3ee2acd0ca6644fc
Lakshamana <guitrompa1@gmail.com>
2019-12-02 23:28:24 -0300

    fixes no callback + make request sync again

--- src/index.js
+++ src/index.js
@@ -6,27 +6,24 @@ function validateForm(evt) {
   console.log(city, state);
 
   const codigo_url = `http://servicos.cptec.inpe.br/XML/listaCidades?city=${city}`
-  getFromUrl(codigo_url, function() {
-    const response = this.responseXML.getElementsByTagName('cidades')
-    let codigo = null
-    for (const cidade of response){
-      for (const node of cidade.childNodes){
-        if (node.childNodes[1].innerHTML === state){
-          codigo = node.childNodes[2].innerHTML
-          document.getElementById('codigo').value = codigo
-          break
-        }
+  const response = getFromUrl(codigo_url).getElementsByTagName('cidades')
+  let codigo = null
+  for (const cidade of response){
+    for (const node of cidade.childNodes){
+      if (node.childNodes[1].innerHTML === state){
+        codigo = node.childNodes[2].innerHTML
+        document.getElementById('codigo').value = codigo
+        break
       }
     }
-  })
+  }
 
-  const codigo = document.getElementById('codigo').value
+  const code = document.getElementById('codigo').value
   console.log(codigo)
-  const previsao_url = `http://servicos.cptec.inpe.br/XML/cidade/${codigo}/previsao.xml`
-  getFromUrl(previsao_url, function() {
-    const response = this.responseXML.getElementsByTagName('previsao')
+  const previsao_url = `http://servicos.cptec.inpe.br/XML/cidade/${code}/previsao.xml`
+  const response2 = getFromUrl(previsao_url).getElementsByTagName('previsao')
     let previsoes = []
-    for (const dia of response){
+    for (const dia of response2){
       let previsao = {}
       for (const node of dia.childNodes){
         previsao[node.tagName] = node.innerHTML
@@ -35,8 +32,6 @@ function validateForm(evt) {
     }
     console.log(previsoes)
     writeDataOnHtml(previsoes)
-  })
-
 }
 
 function writeDataOnHtml(payload){
@@ -69,11 +64,11 @@ function writeDataOnHtml(payload){
   'Para legenda de siglas do tempo, clique <a href="http://servicos.cptec.inpe.br/XML/#condicoes-tempo">aqui</a>'
 }
 
-function getFromUrl(url, cb) {
+function getFromUrl(url) {
   const req = new XMLHttpRequest()
-  req.open('GET', url)
-  req.onload = cb
+  req.open('GET', url, false)
   req.send()
+  return req.responseXML
 }
 
 /**
